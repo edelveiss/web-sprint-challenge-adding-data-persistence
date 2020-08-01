3bbaecc2b35a7128d17a608da50c8d8ce7519be8
Tatiana Zhizhimontova <tatyana.zhi@gmail.com>
2020-07-31 19:06:29 -0700

    find, findById, add, update and delete functions are added in projectModel file and in resource and task model files

--- data/helpers/resourceModel.js
+++ data/helpers/resourceModel.js
@@ -1,8 +1,49 @@
 const db = require("../db-config.js");
 module.exports = {
   find,
+  findById,
+  findProjects,
+  addToProject,
+  add,
+  update,
 };
 
 function find() {
   return db("resources");
 }
+
+function findById(id) {
+  return db("resources").where({ id }).first();
+}
+function findProjects(id) {
+  return db("projects as p")
+    .join("project_resources as pr", "pr.project_id", "p.id")
+    .join("resources as r", "r.id", "pr.resource_id")
+    .select("r.id as resource_id", "p.project_name as project_name")
+    .where("pr.resource_id", id);
+}
+
+function add(resource) {
+  return db("resources")
+    .insert(resource)
+    .then((ids) => {
+      return findById(ids[0]);
+    });
+}
+function addToProject(project_id, resource_id) {
+  return db("project_resources").insert({ project_id, resource_id });
+}
+
+function update(id, changes) {
+  return db("resources")
+    .where({ id })
+    .update(changes)
+    .then((count) => (count > 0 ? findById(id) : null));
+}
+
+// function remove(id) {
+//   return db("resources")
+//     .where("id", id)
+//     .del()
+//     .then((delRow) => (delRow > 0 ? id : null));
+// }
--- data/helpers/taskModel.js
+++ data/helpers/taskModel.js
@@ -1,8 +1,53 @@
 const db = require("../db-config.js");
 module.exports = {
   find,
+  findById,
+  //findProjects,
+  add,
+  update,
+  remove,
 };
 
+// function find() {
+//   return db("tasks");
+// }
 function find() {
-  return db("tasks");
+  return db("tasks as t")
+    .join("projects as p", "t.project_id", "p.id")
+    .select(
+      "p.id as project_id",
+      "p.project_name as project_name",
+      "p.description as project_description",
+      "t.description as task_description",
+      "t.notes as task_notes",
+      "t.completed as task_completed"
+    )
+    .orderBy("p.id");
+  //.where(""t.project_id", id");
+}
+
+function findById(id) {
+  return db("tasks").where({ id }).first();
+}
+
+function add(task) {
+  return db("tasks")
+    .insert(task)
+    .then((ids) => {
+      return findById(ids[0]);
+    });
+}
+
+function update(id, changes) {
+  return db("tasks")
+    .where({ id })
+    .update(changes)
+    .then((count) => (count > 0 ? findById(id) : null));
+}
+
+function remove(id) {
+  return db("tasks")
+    .where("id", id)
+    .del()
+    .then((delRow) => (delRow > 0 ? id : null));
 }
